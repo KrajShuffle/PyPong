60e16f44e7eec76ada6d307345c35d8dbcd0d03a
KrajShuffle <kraj21@berkeley.edu>
2024-02-28 18:10:10 -0800

    Implemented velocity constrained movement & ball overlap response

--- Code/Paddle.py
+++ Code/Paddle.py
@@ -5,25 +5,46 @@ class Paddle:
         self.pos_y = pos_y
         self.pd_width = pd_width
         self.pd_height = pd_height
-        self.hand_ymax = 1350
-        self.hand_ymin = 200
         self.rect = pygame.Rect(self.pos_x, self.pos_y, self.pd_width, self.pd_height)
+        self.can_move = True
+        self.time_pause = 500
+        self.pause_initial_time = 0
+        self.max_velocity = 30
+        self.velocity = 0
+    def vert_collision_response(self, ball):
+        self.can_move = False
+        self.pause_initial_time = pygame.time.get_ticks()
+        # Overlap Area Height between the ball and paddle
+        # Prevent multiple collisions since ball cannot move far enough in next step without still being in the paddle
+        overlap_height = self.rect.clip(ball.rect).height
+        print(overlap_height)
+        if ball.vel_y > 0:
+            self.rect.y += overlap_height
+            print(self.rect.y)
+        else:
+            self.rect.y -= overlap_height
+            print(self.rect.y)
+
     def update_paddle_y(self,hands, game_height, wall_thickness, wiggle_room):
-        # HandDetector
-        if hands:
+        current_time = pygame.time.get_ticks()
+        if (current_time - self.pause_initial_time) >= self.time_pause:
+            self.can_move = True
+        if (hands and self.can_move):
             hand = hands[0]
             x_9, y_9 = hand['lmList'][9]  # Middle Finger MCP
             x_0, y_0 = hand['lmList'][0]  # Wrist
-            # Mapping from approximately hand palm to center of paddle
-            yhand_cam = int(((y_9 + y_0) / 2))
+            # Mapping from approximately palm of hand to center of paddle
+            yhand_cam = int(((y_9 + y_0) / 2) - (self.pd_height / 2))
             max_bottom = game_height - (wall_thickness + wiggle_room + self.pd_height)
             max_top = wall_thickness + wiggle_room
-            if ((yhand_cam < self.hand_ymax) & (yhand_cam > self.hand_ymin)):
-                # Linear Interpolation of Mapping Hand Y Range to Paddle Y Range
-                # Hand Y Range: (200 to 1350)
-                # Paddle Y Range (wall_thickness (40) to (game_height (720) - wall_thickness (40) - paddle_height (100))
-                self.rect.y = int(yhand_cam *(540 / 1150) + 40 - (108_000 / 1150))
-            if not (yhand_cam < self.hand_ymax):
+            if ((yhand_cam < max_bottom) & (yhand_cam > max_top)):
+                new_velocity = yhand_cam - self.rect.y
+                if new_velocity < 0:
+                    self.velocity = max(new_velocity, -1 * self.max_velocity)
+                else:
+                    self.velocity = min(new_velocity, self.max_velocity)
+                self.rect.y += self.velocity
+            if not (yhand_cam < max_bottom):
                 self.rect.y = max_bottom
-            if not(yhand_cam > self.hand_ymin):
+            if not(yhand_cam > max_top):
                 self.rect.y = max_top
