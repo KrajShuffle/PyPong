bbdc7eefbbf27f7b5f0f4a8ee1b005707341dce3
KrajShuffle <kraj21@berkeley.edu>
2024-02-29 13:08:45 -0800

    Added Bad Hit Detection

--- Code/GameMetrics.py
+++ Code/GameMetrics.py
@@ -1,14 +1,46 @@
 import pygame
-
+import numpy as np
 class GameMetrics:
-    def __init__(self, game_hscore):
+    def __init__(self, game_lives,  wall_thickness, screen_height, screen_width):
         self.score = 0
-        self.lives = 3
-        self.game_hscore = game_hscore
-    def increment_score(self):
+        self.lives = game_lives
+        self.contact_time = 0
+        self.ball_velx = 0
+        self.ball_vely = 0
+        self.paddle_center = [0,0]
+        self.wall_thickness = wall_thickness
+        self.screen_height = screen_height
+        self.screen_width = screen_width
+        self.max_time = 0
+    def increment_score(self, pong_ball, paddle):
         self.score += 10
+        self.contact_time = pygame.time.get_ticks() # Get time of impact
+        self.ball_velx = pong_ball.vel_x
+        self.ball_vely = pong_ball.vel_y
+        self.paddle_center = [paddle.centerx, paddle.centery]
     def decrement_lives(self):
         self.lives -= 1
+    def decrement_score(self):
+        self.score -= 10
+    def bad_hit_detector(self):
+        current_time = pygame.time.get_ticks()
+        # Determine Largest Vertical Distance
+        vertical_distance = 0
+        center_topwall_distance = self.paddle_center[1] - self.wall_thickness
+        center_botwall_distance = ((self.screen_height - self.wall_thickness) - self.paddle_center[1])
+        if (center_topwall_distance > center_botwall_distance):
+            vertical_distance = center_topwall_distance
+        else:
+            vertical_distance = center_botwall_distance
+        # Compute max distance traveled if ball just went up to the wall and then right
+        max_distance = vertical_distance + (self.screen_width - self.paddle_center[0])
+        # Compute approximate distance ball would travel if it was a bad hit
+        # bad hit only possible if ball bounces off top or bottom part of paddle to boundary
+        diagonal_speed = np.sqrt((self.ball_vely **2) + (self.ball_velx ** 2))
+        distance_traveled = (current_time - self.contact_time) / diagonal_speed
+
+        if(distance_traveled <= max_distance):
+            self.decrement_score()
     def reset_game(self):
         self.score = 0
         self.lives = 3
