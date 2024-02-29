2376d455f23b35aba4c3f5890afbd5164a1bac91
KrajShuffle <kraj21@berkeley.edu>
2024-02-29 13:57:14 -0800

    Improved & Adaptable ball movement with new background

--- Code/Pong_logic.py
+++ Code/Pong_logic.py
@@ -19,8 +19,8 @@ Pygame Template
 import pygame
 import cv2
 from Paddle import Paddle
-from Ball import Ball
-from PIL import Image
+from BallLife import BallLife
+from GameMetrics import GameMetrics
 import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 
@@ -46,27 +46,35 @@ hand_detect = HandDetector(maxHands= 1)
 
 # Define colors
 white = (255,255,255)
+red = (255, 0, 0)
 green = (0, 255, 0)
 black = (0, 0, 0)
+blue = (0, 0, 255)
+
+# Load Game Font & lives
+titlefont = pygame.font.Font("../Game Assets/Cabal-w5j3.ttf", 50)
+game_lives = 3
 
 # Load Game Images
-print(pygame.image.get_extended())
-heart_img = Image.open("../Game Assets/heart48x48.png")
-pygame_image = pygame.image.fromstring(heart_img.tobytes(), heart_img.size, heart_img.mode).convert_alpha()
+heart_img = pygame.image.load("../Game Assets/heart48x48.png").convert_alpha()
+life_appear = [True] * game_lives
+start_life_pos = 950
+life_pos = start_life_pos
 
 
 # Define the ball & ball velocity
 ball_radius = 20
-ball_pos = [(screen_width // 2), (height // 2)]
+ball_center = [(screen_width // 2) -50, (height // 2)]
 ball_vel = [4, 4]
 ball_speedinc = 2
 ball_maxspeed = int(fps / 2) - 1
-ball = Ball(ball_pos, ball_radius, ball_vel, ball_speedinc, ball_maxspeed)
 
+ball_rect = pygame.Rect(ball_center[0] - ball_radius, ball_center[1] - ball_radius, (2 * ball_radius), (2 * ball_radius))
+ball = BallLife(ball_rect, ball_vel, ball_speedinc, ball_maxspeed)
 # Define paddle and location
 paddle_width = 40
 paddle_height = 100
-paddle_pos = [(screen_width - 150), (height // 2)]
+paddle_pos = [(screen_width - 225), (height // 2)]
 paddle = Paddle(paddle_pos[0], paddle_pos[1],paddle_width, paddle_height)
 
 # Define the walls and their location
@@ -79,6 +87,9 @@ left_wall = pygame.Rect(0, wall_thickness, wall_thickness, height - (2 * wall_th
 walls = [top_wall, bottom_wall, left_wall]
 rects = [top_wall, bottom_wall, left_wall, paddle.rect]
 
+# Initialize Game
+game_stats = GameMetrics(game_lives, wall_thickness, height, screen_width)
+
 # Define collision detection test
 def collide_test(ball, rects):
     collided_rects = []
@@ -88,23 +99,39 @@ def collide_test(ball, rects):
     return collided_rects
 
 def move_ball(ball, coltargets):
-    ball.movex()
-    collide_rects_x = collide_test(ball.rect, coltargets)
-    if len(collide_rects_x) != 0:
-        if paddle.rect in collide_rects_x:
-            ball.paddle_collide()
-        ball.flip_velx()
-        ball.movex()
-    ball.movey()
+    # Move the ball vertically
+    ball.rect.y += ball.vel_y
     collide_rects_y = collide_test(ball.rect, coltargets)
-    if len(collide_rects_y) != 0:
-        if paddle.rect in collide_rects_y:
-            paddle.vert_collision_response(ball)
-            ball.vertical_paddle_collide(paddle.rect)
-            ball.paddle_collide()
+    for obj in collide_rects_y:
+        if ball.vel_y > 0:
+            ball.rect.bottom = obj.top
+            if obj == paddle.rect:
+                paddle.vert_collision_response(ball)
+                ball.paddle_collide()
+                game_stats.increment_score(ball, paddle.rect)
+        elif ball.vel_y < 0:
+            ball.rect.top = obj.bottom
+            if obj == paddle.rect:
+                paddle.vert_collision_response(ball)
+                ball.paddle_collide()
+                game_stats.increment_score(ball, paddle.rect)
         ball.flip_vely()
-        ball.movey()
 
+    # Move the ball horizontally
+    ball.rect.x += ball.vel_x
+    collide_rects_x = collide_test(ball.rect, coltargets)
+    for obj in collide_rects_x:  # for each object collided with ball in collide_rects_x
+        if ball.vel_x > 0:
+            ball.rect.right = obj.left
+            if obj == paddle.rect:
+                ball.paddle_collide()
+                game_stats.increment_score(ball, paddle.rect)
+        elif ball.vel_x < 0:
+            ball.rect.left = obj.right
+            if obj == paddle.rect:
+                ball.paddle_collide()
+                game_stats.increment_score(ball, paddle.rect)
+        ball.flip_velx()
 
 # Main Loop
 start = True
@@ -121,7 +148,14 @@ while start:
 
     # if ball is about to exceed left or right most bounds; kept for testing game
     if ((ball.pos_x - ball_radius) <= 0) or ((ball.pos_x + ball_radius) >= screen_width):
-        ball.flip_velx()
+        # Reinitialize ball at original position
+        ball_rect = pygame.Rect(ball_center[0] - ball_radius, ball_center[1] - ball_radius, (2 * ball_radius),
+                                (2 * ball_radius))
+        ball = BallLife(ball_rect, ball_vel, ball_speedinc, ball_maxspeed)
+        # Game Penalty Actions
+        game_stats.bad_hit_detector()
+        game_stats.decrement_lives() # 3 to 0
+        life_appear[game_stats.lives] = False
 
 
     # Creating and loading in webcam image
@@ -153,11 +187,24 @@ while start:
     # Overlaid Elements in Order
     game_window.blit(black_bg, (0, 0))  # Adding black background as the base
     game_window.blit(cam_surface, (0, 0)) # Adding transparent Canvas on top of black bg
-    pygame.draw.circle(game_window, white, [ball.pos_x, ball.pos_y], ball.radius)  # white ball
+    pygame.draw.circle(game_window, white,
+                       [ball.pos_x + ball_radius, ball.pos_y + ball_radius], ball_radius)  # white ball
     pygame.draw.rect(game_window, green, paddle.rect)
     for wall in walls:
         pygame.draw.rect(game_window, white, wall)
-    game_window.blit(pygame_image, (200, 0))
+
+    # Text and Images
+    gameTitle = titlefont.render("Pong Game", False, black)
+    game_window.blit(gameTitle, (wall_thickness, 0))
+    scoreLabel = titlefont.render("Score: " + str(game_stats.score), False, blue)
+    game_window.blit(scoreLabel, (wall_thickness + 575, 0))
+
+    for logical in life_appear:
+        if logical:
+            game_window.blit(heart_img, (life_pos, 12))
+        life_pos += 55
+    life_pos = start_life_pos # Redefined to prevent displaying outside display
+
     # Update display
     pygame.display.update()
 
